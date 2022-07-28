c0bdffebc42d8a8ea4039d78bf452b2afd980f89
Jacob Godwin <jpg174@gmail.com>
2022-07-28 14:00:19 -0400

    finish intro

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function () {
+//   console.log(window.pageYOffset);
+//   hideNav();
+// };
 
+// // Get navbar element
+// var navbar = document.getElementById('navbar');
+
+// // Get the intro div height
+// var intro = document.getElementById('intro');
+
+// // Get the offset position of the intro div
+// var sticky = 100;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function hideNav() {
+//   if ((window.pageYOffset = sticky)) {
+//     navbar.style = 'position:sticky';
+//   }
+// }
